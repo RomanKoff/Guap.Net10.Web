84edcf0b9cbbb840b5ebd0bda61e94caa73ed0c3
Roman Koff <krv@guap.ru>
2026-02-12 23:28:46 +0300

    2026-002-12

--- Services/GuapClaimsTransformation.cs
+++ Services/GuapClaimsTransformation.cs
@@ -1,4 +1,5 @@
 ﻿using Ans.Net10.Common;
+using Ans.Net10.Web;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.Extensions.Configuration;
 using System.Security.Claims;
@@ -38,20 +39,32 @@ namespace Guap.Net10.Web.Services
 					principal.GetNameIdentifierFromClaim());
 				if (profile1 != null)
 				{
+
 					// right
 					principal.AddClaims(
 						Ans.Net10.Web._Consts.CLAIM_AUTH_POLICY_TYPE,
 						profile1.Right.ToString());
+
 					// roles
 					if (!string.IsNullOrEmpty(profile1.Roles))
-						principal.AddClaimsRoles(
+						principal.AddRolesClaims(
 							profile1.Roles.Split(';'));
+
 					// actions
 					if (!string.IsNullOrEmpty(profile1.Actions))
+					{
+						var helper1 = new MvcAccessHelper(profile1.Actions);
 						principal.AddClaims(
 							Ans.Net10.Web._Consts.CLAIM_ACTIONS_TYPE,
-							profile1.Actions.Split(';'));
-					// props
+							helper1.GetResultClaimsValue());
+					}
+
+					// resources
+					if (!string.IsNullOrEmpty(profile1.Resources))
+						principal.AddClaims(
+							Ans.Net10.Web._Consts.CLAIM_RESOURCES_TYPE,
+							profile1.Resources.Split(';'));
+
 				}
 			}
 			return Task.FromResult(principal);
